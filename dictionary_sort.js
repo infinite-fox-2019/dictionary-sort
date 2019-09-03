e87aceca5c7604bffcc4f60ee062d741541545c3
ashirogisen <m.rizki.aiman@gmail.com>
2019-09-03 12:41:11 +0700

    uploaded

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,6 +1,20 @@
 function dictionarySort(dictionaries) {
   // your code below here...
+  for (var i = 0; i < dictionaries.length-1; i++) {
+    for (var j = 0; j < dictionaries.length-i-1; j++) {
+        if (dictionaries[j] > dictionaries[j+1]) {
+            var temp = dictionaries[j];
+            dictionaries[j] = dictionaries[j+1];
+            dictionaries[j+1] = temp;
+        }
+    }
+  }
+
+  return dictionaries;
 
 }
+
+console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang']));
+console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi']));
 
 module.exports = dictionarySort
