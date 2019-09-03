ffb7d420f39e8e0ec0e65ff87b2a72b526caa48c
aldinofrizal <aldinofrizal@gmail.com>
2019-09-03 16:38:00 +0700

    dictionary sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,6 +1,18 @@
 function dictionarySort(dictionaries) {
   // your code below here...
+  for (var i = 0; i < dictionaries.length-1; i++)
+      if(dictionaries[i]>dictionaries[i+1])dictionaries.splice(i,2,dictionaries[i+1],dictionaries[i])
+  for (var i = 0; i < dictionaries.length-1; i++)
+      if(dictionaries[i] > dictionaries[i+1])return dictionarySort(dictionaries)
+  return dictionaries
 
 }
 
+// console.log(Infinity < 'ab' , 'infinity lebih kecil dairpada a')
+
+
+
 module.exports = dictionarySort
+
+console.log(dictionarySort(['makan','duduk','tidur','terbang']))
+console.log(dictionarySort(['anggi','angga','ani','adi']))
