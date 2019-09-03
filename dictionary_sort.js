9f9747b131126841ed2aea29915c2620b4084228
arnold_therigan <arnoldtherigan15@gmail.com>
2019-09-03 18:35:39 +0700

    update dictionarysort.js

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -2,11 +2,11 @@ function dictionarySort(dictionaries) {
   for (var i = 0; i < dictionaries.length; i++) {
     for (var j = i+1; j < dictionaries.length;j++) {
       if (dictionaries[j] < dictionaries[i]) {
-        [dictionaries[j],dictionaries[i]] = [dictionaries[i],[dictionaries[j]]]
+        [dictionaries[j],dictionaries[i]] = [dictionaries[i],dictionaries[j]]
       }
     }
   }
-  return dictionaries;
+  return dictionaries.join(',');
 }
 console.log(dictionarySort(['makan','duduk','tidur','terbang']));
 // duduk , makan, terbang , tidur
