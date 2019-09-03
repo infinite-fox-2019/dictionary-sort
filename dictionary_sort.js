1decdc33b88de175b39b64d62e81c365690e6c6f
Peter Lee <peter.lee230597@gmail.com>
2019-09-03 20:06:43 +0700

    dictionary sort done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,6 +1,28 @@
 function dictionarySort(dictionaries) {
   // your code below here...
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
+  for(i=0 ; i<dictionaries.length; i++){
+    for(j=0 ; j<dictionaries.length ; j++){
+      if(dictionaries[j] > dictionaries[j+1]){
+        var temp = dictionaries[j]
+        dictionaries[j] = dictionaries[j+1]
+        dictionaries[j+1] = temp
+      }
+    }
+
+  }
+  return dictionaries
 }
 
-module.exports = dictionarySort
+console.log(dictionarySort(['l', 'z', 'm']))
+
+console.log(dictionarySort(['annie', 'anny']))
+
+console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang']))
+// // output: duduk, makan, terbang, tidur
+
+console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi']))
+// //output: adi angga anggi ani
+
+// module.exports = dictionarySort
