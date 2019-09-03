251968a5519830fd2acc7e14bffb2a5719906962
straf16 <depoksugutamu621@gmail.com>
2019-09-03 15:45:08 +0700

    mengerjakan dictionary sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,6 +1,21 @@
 function dictionarySort(dictionaries) {
   // your code below here...
-
+  for (var i = 0; i < dictionaries.length; i++) {
+    var indexMinimum = i;
+    for (var j = i; j < dictionaries.length; j++) {
+      if (dictionaries[j]<dictionaries[indexMinimum]) {
+        indexMinimum = j;
+      }
+    }
+    var temp = dictionaries[i];
+    dictionaries[i] = dictionaries[indexMinimum];
+    dictionaries[indexMinimum] = temp;
+  }
+  return dictionaries.join(',');
 }
+
+console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang']));
+console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi']));
+
 
 module.exports = dictionarySort
