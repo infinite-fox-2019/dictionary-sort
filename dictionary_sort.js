7042f5e193bf95b91b2828d72fea46617a0d7046
mardiansn <mardiansyah.011@gmail.com>
2019-09-03 21:29:28 +0700

    done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,6 +1,16 @@
 function dictionarySort(dictionaries) {
   // your code below here...
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j + 1] < arr[j]) {
+        let temp = arr[j + 1]
+        arr[j + 1] = arr[j]
+        arr[j] = temp
+      }
+    }
+  }
 
+  return arr
 }
 
 module.exports = dictionarySort
