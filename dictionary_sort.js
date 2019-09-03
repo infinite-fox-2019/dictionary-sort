73d0172cda49667d60882aa63e8e726aeda3bcdf
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-09-03 18:26:14 +0700

    dictSort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,6 +1,20 @@
 function dictionarySort(dictionaries) {
-  // your code below here...
-
+  // var kamus = 'abcdefghijklmnopqrstuvwxyz'
+  var temp = ''
+  for (var i=0; i<dictionaries.length; i++) {
+    for (var j=i+1; j<dictionaries.length; j++) {
+      if (dictionaries[i] > dictionaries[j]) {
+          temp = dictionaries[j]
+          dictionaries[j] = dictionaries[i]
+          dictionaries[i] = temp
+      }
+    }
+  }
+  return dictionaries
 }
 
-module.exports = dictionarySort
+var arrOfWord = ['makan', 'duduk', 'tidur', 'terbang'] //duduk, makan, terbang, tidur
+var arrOfWord = ['anggi', 'angga', 'ani', 'adi'] //angga, anggi, adi, ani
+console.log(dictionarySort(arrOfWord));
+
+// module.exports = dictionarySort
