f9d66c15c1d3faa286a5a404491d020ad9200e73
Ricky <audrickong.ao@gmail.com>
2019-09-03 14:53:21 +0700

    Uploading

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,6 +1,26 @@
-function dictionarySort(dictionaries) {
-  // your code below here...
+function dictionarySort(arr){
+	var alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+	for(var i = 1 ; i < arr.length ; i++){
+		var current = arr[i]
+		for(var j = i - 1 ; j >= 0 ; j--){
+			for(var k = 0 ; k < arr[j].length ; k++){
+				if(arr[j][k]!==current[k]){
+					if(alphabet.indexOf(arr[j][k])>alphabet.indexOf(current[k])){
+            var store = arr[j + 1]
+            arr[j + 1] = arr[j]
+            arr[j] = store
+					}
+					break
+				}
+			}
+		}
+	}
+	return arr
 }
 
+console.log(dictionarySort(["makan", "duduk", "terbang", "tidur"])); 
+// duduk,makan,terbang,tidur
+console.log(dictionarySort(["angga", "ani", "adi", "anggi"]));
+// angga,anggi,adi,ani
 module.exports = dictionarySort
