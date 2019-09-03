b29116fc5641e0ae0bfcc3c09ac33ebe2ee57d52
dwitama alfred <dwitamaalfred@dwitamas-MacBook-Pro.local>
2019-09-03 18:37:25 +0700

    dictionary sort done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,6 +1,54 @@
 function dictionarySort(dictionaries) {
-  // your code below here...
+//selection sort
 
+  for(var i =0; i<dictionaries.length; i++){
+    var min = dictionaries[i]
+    var index = -1
+    for(var j = i+1; j<dictionaries.length; j++){
+      if(dictionaries[j] < min){
+        min = dictionaries[j]
+        index = j
+      }
+    }
+    if(dictionaries[i] > dictionaries[index]){
+      var temp = dictionaries[index]
+      dictionaries[index] = dictionaries[i]
+      dictionaries[i] = temp
+    }
+  }
+  return dictionaries
 }
-
+console.log(dictionarySort(['makan','duduk','tidur','terbang']))
+console.log(dictionarySort(['anggi','angga','ani','adi']))
 module.exports = dictionarySort
+
+
+
+//bubble sort
+
+// function dictionarySort(dictionaries) {
+//   for(var i =0; i<dictionaries.length; i++){
+//     for(var j = i+1; j<dictionaries.length; j++){
+//       if(dictionaries[i] > dictionaries[j]){
+//         var temp = dictionaries[j]
+//         dictionaries[j] = dictionaries[i]
+//         dictionaries[i] = temp
+//       }
+//     }
+//   }
+// return dictionaries
+// }
+
+//insertion
+//   for(var i = 1; i<dictionaries.length; i++){
+//     var start = dictionaries[i]
+//     for(var j = i-1; j>=0; j--){
+//       if(dictionaries[j] > start){
+//         var temp = dictionaries[j+1]
+//         dictionaries[j+1] = dictionaries[j]
+//         dictionaries[j] = temp
+//       }
+//     }
+//   }
+//   return dictionaries
+// }
