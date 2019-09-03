ffbdf8c19cdba63c65518f8e80f225d0cca2412c
adityain98 <adityain98@gmail.com>
2019-09-03 17:30:31 +0700

    selesai

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,6 +1,24 @@
 function dictionarySort(dictionaries) {
-  // your code below here...
-
+  for(var i = 0; i<dictionaries.length; i++){
+    if(i !== 0){
+      for(var j = i+1; j>=0; j--){
+        if(dictionaries[j] <= dictionaries[j-1]){
+          var swap = dictionaries[j];
+          dictionaries[j] = dictionaries[j-1];
+          dictionaries[j-1] = swap;
+        }
+      }
+    }
+  }
+  return dictionaries;
 }
+var arr = ['makan', 'duduk', 'tidur', 'terbang'];
+var arr2 = ['ari', 'ara', 'ani']
+
+console.log(dictionarySort(arr));
+console.log(dictionarySort(arr2));
 
 module.exports = dictionarySort
+
+
+
