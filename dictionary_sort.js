102526d1d88762989061fab1b3e5a4680da1d233
devitasari <devitas700@gmail.com>
2019-09-03 15:48:34 +0700

    add new file

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,6 +1,14 @@
 function dictionarySort(dictionaries) {
   // your code below here...
-
+  for (var i=0; i<dictionaries.length; i++) {
+    for (var j=0; j<dictionaries.length; j++) {
+      if (dictionaries[j] > dictionaries[j+1]) {
+        [dictionaries[j],dictionaries[j+1]] = [dictionaries[j+1],dictionaries[j]]
+      }
+    }
+  }
+  return dictionaries.join(',')
 }
-
+console.log(dictionarySort(['makan','duduk','tidur','terbang']))
+console.log(dictionarySort(['anggi','angga','ani','adi']))
 module.exports = dictionarySort
