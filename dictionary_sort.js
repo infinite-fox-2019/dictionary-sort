808cfe7776adabe1963080e1dbdfb8d1161ebf1d
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-09-03 15:55:21 +0700

    add solution

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,6 +1,18 @@
 function dictionarySort(dictionaries) {
-  // your code below here...
-
+  for(var i = 1; i < dictionaries.length; i++) {
+    var currentElement = dictionaries[i];
+    for(var j = i - 1; j >= 0; j--) {
+      if(dictionaries[j] > currentElement) {
+        dictionaries[j + 1] = dictionaries[j];
+        dictionaries[j] = currentElement;
+      }
+    }
+  }
+  return dictionaries;
 }
 
+console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang']));
+console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi']));
+console.log(dictionarySort(['delta', 'beta', 'alpha', 'charlie']));
+console.log(dictionarySort([]));
 module.exports = dictionarySort
