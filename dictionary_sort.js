5e384aa66ca2a8e0c79fbce02d9fe6e250537851
Kevin Anantha <kevin.anantha@gmail.com>
2019-09-03 16:15:11 +0700

    :anchor:

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,6 +1,16 @@
 function dictionarySort(dictionaries) {
-  // your code below here...
-
+  for (let i = 0; i < dictionaries.length; i) {
+    if (dictionaries[i] > dictionaries[i + 1]) {
+      const temp = dictionaries[i + 1]
+      dictionaries[i + 1] = dictionaries[i]
+      dictionaries[i] = temp
+      i = Math.max(0, i - 1)
+    } else i++
+  }
+  return dictionaries
 }
+
+console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang']))
+console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi']))
 
 module.exports = dictionarySort
