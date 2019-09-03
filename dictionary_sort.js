2d27dbd0189934bdb5dbfb683da8da3711b4c15b
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-09-03 17:02:06 +0700

    dictionary_nadhil

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,6 +1,20 @@
 function dictionarySort(dictionaries) {
-  // your code below here...
-
+for (var i = 0; i < dictionaries.length;i++){
+  for (var j = 0; j < dictionaries.length;j++){
+    if (dictionaries[i]<dictionaries[j]){
+      var x = dictionaries[i]
+      dictionaries[i] = dictionaries[j]
+      dictionaries[j] = x
+    }
+  }
 }
+return dictionaries;
+}
+
+var arr1 = ['makan','duduk','tidur','terbang']
+var arr2 = ['anggi','angga','ani','adi']
+
+console.log(dictionarySort(arr1))
+console.log(dictionarySort(arr2))
 
 module.exports = dictionarySort
