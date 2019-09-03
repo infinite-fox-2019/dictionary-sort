9d99f4246204ca5a3d2448611bbdb7ece0e1ffd3
hoandreas <andreas_ho@hotmail.com>
2019-09-03 15:36:35 +0700

    submit tugas dictionary sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,6 +1,16 @@
 function dictionarySort(dictionaries) {
-  // your code below here...
-
+  for(var i = 0; i < dictionaries.length; i++) {
+    for(var j = i+1; j < dictionaries.length; j++) {
+      if(dictionaries[j] < dictionaries[i]) {
+        var temp = dictionaries[j];
+        dictionaries[j] = dictionaries[i];
+        dictionaries[i] = temp;
+      }
+    }
+  }
+  return dictionaries;
 }
 
-module.exports = dictionarySort
+console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang']))
+console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi']))
+// module.exports = dictionarySort
