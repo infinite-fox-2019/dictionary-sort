a2045bce8ccb8d3328830a87b5b28e0ae22217fc
Panji SN <panji1595@gmail.com>
2019-09-03 15:41:34 +0700

    tugas dictionary

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,6 +1,20 @@
 function dictionarySort(dictionaries) {
-  // your code below here...
+  for (var i = 0; i<dictionaries.length;i++){
+    for (var j = i ; j>=0;j--){
+      if (dictionaries[j+1]<dictionaries[j]){
+        var temp = dictionaries[j];
+        dictionaries[j] = dictionaries[j+1];
+        dictionaries[j+1] =temp;
+      }
+    }
+  }
+  return dictionaries.join(",");
 
 }
 
+var arrOfWord1 = ["makan","duduk","tidur","terbang"];
+var arrOfWord2 = ["anggi","angga","ani","adi"]
+
+console.log(dictionarySort(arrOfWord1));
+console.log(dictionarySort(arrOfWord2));
 module.exports = dictionarySort
